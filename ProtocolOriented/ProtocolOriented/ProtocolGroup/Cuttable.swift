a830b9bf8b1f7eba8e219238383fe7007952c2e8
Sunil-Kumar1 <Sunil.Kumar1@timesinternet.in>
2020-01-23 11:05:34 +0530

    added fireable and cuttable protocol

---
+++ ProtocolOriented/ProtocolOriented/ProtocolGroup/Cuttable.swift
@@ -0,0 +1,13 @@
+//
+//  Cuttable.swift
+//  ProtocolOriented
+//
+//  Created by sunil.kumar1 on 1/23/20.
+//  Copyright © 2020 sunil.kumar1. All rights reserved.
+//
+
+import Foundation
+protocol Cuttable {
+    var weight : Double {get}
+    var steel : String {get}
+}
---
+++ ProtocolOriented/ProtocolOriented/ProtocolGroup/Fireable.swift
@@ -0,0 +1,13 @@
+//
+//  Fireable.swift
+//  ProtocolOriented
+//
+//  Created by sunil.kumar1 on 1/23/20.
+//  Copyright © 2020 sunil.kumar1. All rights reserved.
+//
+
+import Foundation
+
+protocol Fireable {
+    var magazineSize: Int {get}
+}
