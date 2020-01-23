a2f0bae0af64762dbb963792ea968a41fdcf006c
Sunil-Kumar1 <Sunil.Kumar1@timesinternet.in>
2020-01-23 15:19:08 +0530

    class only protocol

---
+++ ProtocolOriented/ProtocolOriented/ProtocolDepthKnowledge/ClassOnlyProtocol.swift
@@ -0,0 +1,24 @@
+//
+//  ClassOnlyProtocol.swift
+//  ProtocolOriented
+//
+//  Created by sunil.kumar1 on 1/23/20.
+//  Copyright © 2020 sunil.kumar1. All rights reserved.
+//
+
+import Foundation
+
+protocol ClassOnlyProtocol : AnyObject {
+    func show()
+}
+
+class P: ClassOnlyProtocol {
+    func show() {
+        
+    }
+}
+
+struct Q : ClassOnlyProtocol { // Error: Non-class type 'Q' cannot conform to class protocol 'ClassOnlyProtocol'
+    
+}
+
